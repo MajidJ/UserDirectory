e3dccc5afbc10393c0283182f9902d43c9babaa2
Majid Jamaleldine <majid.jamaleldine@gmail.com>
2018-11-16 23:16:14 -0600

    fixed mongoDB port problem

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 3005;
 
 // Middleware
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('frontend/build'));
+	app.use(express.static('client/build'));
 }
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
